31c432179e1f2bf01d1ea2fc11d1abbf400359c3
Kit at Home <kit@reapermini.com>
2022-11-07 13:18:18 +0000

    ready to fix off screen position locator

--- Engine/off_screen_position_indicator.gd
+++ Engine/off_screen_position_indicator.gd
@@ -1,6 +1,6 @@
 extends Polygon2D
 
-const MARGIN := Vector2(100,100)
+const MARGIN := Vector2(150,150)
 
 @onready var Canvas := get_parent()
 
@@ -18,33 +18,27 @@ func can_be_seen(_mode) -> void:
 func update_position() -> void:
 	# to find a vector pointing from a -> b, use b-a
 	if visible:
-		
+
 		var rect = get_viewport_rect()
-		
+
 		var rel = (Globals.Things[0].global_position - Globals.player_instance.global_position)
 		var quad = Vector2(sign(rel.x), sign(rel.y))
-		var extents = ((rect.size - MARGIN) / 2)
-		var extents_dir = extents * quad
-		
+		var extents = ((rect.size - MARGIN) / 2) * quad
+
 		var m = rel.y/rel.x
 		var y = (m * extents.x)
 		var x = (extents.y/m)
-
 		
+		var distance = Globals.player_instance.global_position.distance_to(Globals.Things[0].global_position)
+		
+		var d_min = abs(extents.x)
+		var d_scale = d_min/(distance - 100)
+		
+		scale = clamp(Vector2(1,1) * d_scale, Vector2(0.6,0.6), Vector2(1,1))
+
 		if abs(y) >= abs(extents.y):
 			y = m * x
 		else:
 			x = y/m
-#		print("X: ",x, " Y: ", y)
-#		global_position = Globals.player_instance.global_position + Vector2(x,y)
-		position = (Vector2(x,y) * quad) + (extents)
-		print("----------")
-		print(Vector2(x,y))
-		print(quad)
-		print(Vector2(x,y) * quad)
-		print(extents)
-		print(extents_dir)
-		print(position)
-#		global_position = (Globals.player_instance.global_position + Vector2(x,y))
-		
-		rotation = Vector2.ZERO.angle_to_point(rel)
+		global_position = Globals.player_instance.global_position + Vector2(x,y)
+		look_at(Globals.Things[0].global_position)
